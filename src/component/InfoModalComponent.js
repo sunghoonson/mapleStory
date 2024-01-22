c5fecc1a455a008c8089fb79459d0d0c808ba61e
sunghoonson <ssh910525@gmail.com>
2024-01-22 13:38:35 +0900

    modified:   src/component/InfoModalComponent.js 	new file:   src/component/css/DetailModalComponent.css 	modified:   src/component/css/InfoModalComponent.css

--- src/component/InfoModalComponent.js
+++ src/component/InfoModalComponent.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import './css/InfoModalComponent.css';
+import './css/DetailModalComponent.css';
 import { closeModal } from '../features/myModalSlice';
 import Draggable from 'react-draggable';
 
@@ -9,7 +10,13 @@ function ModalComponent() {
   const handleClose = () => {
     dispatch(closeModal({modalName: 'infomodal'}));
   };
+  // showDetail 상태와 상태를 설정하는 함수 setShowDetail을 추가합니다.
+  const [showDetail, setShowDetail] = useState(false);
+  const handleDetailClick = () => {
+    setShowDetail(!showDetail); // 디테일 버튼 클릭 시 상태를 true로 변경하여 모달을 표시
+  };
 
+  const { data} = useSelector((state) => state.myApi);
   return (
     
     <Draggable handle=".infomodal-header">
@@ -20,20 +27,36 @@ function ModalComponent() {
             <div><button onClick={handleClose}>X</button></div>
           </div>
           <div className="infomodal-content">
-          <div className="infogrid-item1">
-            {/* 첫 번째 영역의 내용 */}
+            <div className="infogrid-item1">
+              {/* 첫 번째 영역의 내용 */}
+            </div>
+            <div className="infogrid-item2">
+              {/* 두 번째 영역의 내용 */}
+              <div className="character-level-box">Lv.<strong>{data.character_level}</strong></div>
+              <div>{data.character_image && <img src={data.character_image} alt="Character" />}</div>
+              <div className="character-name-box">{data.character_name}</div>
+            </div>
+            <div className="infogrid-item3">
+              {/* 세 번째 영역의 내용 */}
+            </div>
+            <div>
+              <div className="infomodal-detail-button" onClick={handleDetailClick}>DETAIL</div>
+            </div>
           </div>
-          <div className="infogrid-item2">
-            {/* 두 번째 영역의 내용 */}
-          </div>
-          <div className="infogrid-item3">
-            {/* 세 번째 영역의 내용 */}
-          </div>
-          </div>
+          {showDetail && <DetailModal/>}
         </div>
       </div>
     </Draggable>
-    
+  );
+}
+
+// 디테일 모달 컴포넌트
+function DetailModal({ onClose }) {
+  return (
+    <div className="detail-modal">
+      <div className="infomodalcharacter-info">Character Info</div>
+      {/* 여기에 자세한 정보를 표시하는 내용을 넣습니다. */}
+    </div>
   );
 }
 
