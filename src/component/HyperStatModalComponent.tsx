8307c60e080370324cb84c5e8cbf480cd6f4f707
sunghoonson <ssh910525@gmail.com>
2024-02-02 16:07:10 +0900

    modified:   src/component/HyperStatModalComponent.tsx

--- src/component/HyperStatModalComponent.tsx
+++ src/component/HyperStatModalComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector  } from 'react-redux';
 import './css/HyperStatModalComponent.css?after';
 import { HyperStatModalProps } from './types';
@@ -58,6 +58,7 @@ const HyperStatDisplay = ({ stat }) => {
 // 하이퍼스탯 모달 컴포넌트
 export function HyperStatModal({ data }: HyperStatModalProps) {
   const dispatch = useDispatch();
+  const modalRef = useRef<HTMLDivElement>(null);
   const handleClose = () => {
     dispatch(closeModal({modalName: 'hyperstatmodal'}));
   };
@@ -67,8 +68,25 @@ export function HyperStatModal({ data }: HyperStatModalProps) {
                                 "크리티컬 데미지","방어율 무시","데미지","보스 몬스터 공격 시 데미지 증가",
                                 "상태 이상 내성","공격력/마력","획득 경험치","아케인 포스","일반 몬스터 공격 시 데미지 증가"];
 
+  useEffect(() => {
+    // 모달 영역 밖 클릭을 감지하는 함수
+    function handleClickOutside(event: MouseEvent) {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        dispatch(hideTooltip());
+      }
+    }
+
+    // 전역 클릭 이벤트 리스너 등록
+    document.addEventListener('click', handleClickOutside);
+
+    // 클린업 함수에서 이벤트 리스너 제거
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [dispatch]); // dispatch를 의존성 배열에 추가
+                              
   return (
-    <div className="hyperstat-modal">
+    <div className="hyperstat-modal" ref={modalRef}>
       <div className="hyperstat-header">
         <div className="hyperstatmodaltext-info">Hyper Stat</div>
         <div><button onClick={handleClose}>X</button></div>
